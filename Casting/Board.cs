2d2f77060ec5b346aa070bfaa0a7d83e8fd8e059
Trevor Gibb <91558253+gib-student@users.noreply.github.com>
2023-09-14 13:59:50 -0600

    Update Board.cs

--- Casting/Board.cs
+++ Casting/Board.cs
@@ -4,6 +4,8 @@ namespace chess_program.Casting
 {
     public abstract class Board
     {
+        // A board is an 8x8 collection of spots
+        
         public Board()
         {
             
